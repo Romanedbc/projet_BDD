93f3f4a7df8cb1978bc3711fe8338feef8a9c88e
LouiseBarthon <117666030+LouiseBarthon@users.noreply.github.com>
2023-04-23 20:30:09 +0200

    Add files via upload

--- test trigger verif kine.sql
+++ test trigger verif kine.sql
@@ -1,4 +1,4 @@
-CREATE OR REPLACE PROCEDURE test_trigger_verif_neuro IS
+CREATE OR REPLACE PROCEDURE test_trigger_verif_kine IS
 
 BEGIN
 
@@ -7,7 +7,7 @@ CREATE TABLE medecin_auxiliaire (
     num_adeli VARCHAR2(10),
     specialite VARCHAR2(15)
     nom_medecin VARCHAR2(25),
-    prenom_medecin VARCHAR2(25),
+    prenom_medecin VARCHAR2(25)
 );
 
 -- Insérer des données de test dans la table medecin_auxiliaire
@@ -15,36 +15,38 @@ INSERT INTO medecin_auxiliaire (num_adeli, specialite, nom_medecin,prenom_medeci
 INSERT INTO medecin_auxiliaire (num_adeli, specialite, nom_medecin,prenom_medecin) VALUES ('2345678901', 'biologiste','Hutchinson','Jessica');
 INSERT INTO medecin_auxiliaire (num_adeli, specialite, nom_medecin,prenom_medecin) VALUES ('3456789012', 'kine','Ritter','Tanya');
 
--- Créer une table de test pour les analyses EEG
-CREATE TABLE analyse_EEG (
-    id_analyse_eeg NUMBER,
-    res_electro NUMBER,
+-- Créer une table de test pour les tests d effort
+CREATE TABLE analyse_effort (
+    id_analyse_effort NUMBER,
+    rythme_av_effort NUMBER,
+    rythme_ap_effort NUMBER,
+    rythme_av_repos NUMBER,
     num_adeli VARCHAR2(10)
 );
 
--- Insérer une ligne avec un médecin neurologue
-INSERT INTO analyse_EEG (id_analyse_eeg, res_electro, num_adeli) VALUES (1,4, '1234567890'); --numero adeli correspondant au neurologue Scott Joel
+-- Insérer une ligne avec un médecin kine
+INSERT INTO analyse_effort (id_analyse_effort, rythme_av_effort,rythme_ap_effort,rythme_av_repos, num_adeli) VALUES (1,4,5,6,'3456789012'); --numero adeli correspondant au kine Ritter Tanya
 
 
 -- Insérer une ligne avec un médecin non-neurologue
 BEGIN
-    INSERT INTO analyse_EEG (id_analyse_eeg, res_electro, num_adeli) VALUES (1,2, '2345678901'); --numero adeli correspondant à un medecin biologiste Hutchinson Jessica
+    INSERT INTO analyse_effort (id_analyse_effort, rythme_av_effort,rythme_ap_effort,rythme_av_repos, num_adeli) VALUES (1,4,5,6,'2345678901'); --numero adeli correspondant au biologiste Hutchinson Jessica
 EXCEPTION
     WHEN OTHERS THEN
-        DBMS_OUTPUT.PUT_LINE('Erreur : le médecin n est pas neurologue' || SQLCODE);
+        DBMS_OUTPUT.PUT_LINE('Erreur : le médecin n est pas kine' || SQLCODE);
 END;
 
 
 -- Insérer une ligne avec un médecin inconnu
 BEGIN
-    INSERT INTO analyse_EEG (id_analyse_eeg, res_electro, num_adeli) VALUES (1,3, '9999999999');
+    INSERT INTO analyse_EEG (id_analyse_effort, rythme_av_effort,rythme_ap_effort,rythme_av_repos, num_adeli) VALUES (1,4,5,6,'9999999999');
 EXCEPTION
     WHEN OTHERS THEN
         DBMS_OUTPUT.PUT_LINE('Erreur : aucun medecin ne correspond a ce numero adeli ' || SQLCODE );
 END;
 
 -- Vérifier que la seule seule ligne valide a bien été insérée dans la table analyse_EEG
-SELECT * FROM analyse_EEG;
+SELECT * FROM analyse_effort;
 
 END;
 /
