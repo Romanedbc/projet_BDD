26cc05282770395351fef3d0138db5348a62eb81
LouiseBarthon <117666030+LouiseBarthon@users.noreply.github.com>
2023-04-24 09:20:45 +0200

    Update test trigger verif neuro.sql

--- test trigger verif neuro.sql
+++ test trigger verif neuro.sql
@@ -2,49 +2,33 @@ CREATE OR REPLACE PROCEDURE test_trigger_verif_neuro IS
 
 BEGIN
 
--- Créer une table de test pour les médecins auxiliaires
-CREATE TABLE medecin_auxiliaire (
-    num_adeli VARCHAR2(10),
-    specialite VARCHAR2(15)
-    nom_medecin VARCHAR2(25),
-    prenom_medecin VARCHAR2(25),
-);
-
 -- Insérer des données de test dans la table medecin_auxiliaire
-INSERT INTO medecin_auxiliaire (num_adeli, specialite, nom_medecin,prenom_medecin) VALUES ('1234567890', 'neurologue','Scott','Joel');
-INSERT INTO medecin_auxiliaire (num_adeli, specialite, nom_medecin,prenom_medecin) VALUES ('2345678901', 'biologiste','Hutchinson','Jessica');
-INSERT INTO medecin_auxiliaire (num_adeli, specialite, nom_medecin,prenom_medecin) VALUES ('3456789012', 'kine','Ritter','Tanya');
+INSERT INTO medecin_auxilaire (num_adeli, specialite, nom_medecin,prenom_medecin) VALUES ('1234567890', 'neurologue','Scott','Joel');
+INSERT INTO medecin_auxilaire (num_adeli, specialite, nom_medecin,prenom_medecin) VALUES ('2345678901', 'biologiste','Hutchinson','Jessica');
+INSERT INTO medecin_auxilaire (num_adeli, specialite, nom_medecin,prenom_medecin) VALUES ('3456789012', 'kine','Ritter','Tanya');
 
--- Créer une table de test pour les analyses EEG
-CREATE TABLE analyse_EEG (
-    id_analyse_eeg NUMBER,
-    res_electro NUMBER,
-    num_adeli VARCHAR2(10)
-);
+
 
 -- Insérer une ligne avec un médecin neurologue
-INSERT INTO analyse_EEG (id_analyse_eeg, res_electro, num_adeli) VALUES (1,4, '1234567890'); --numero adeli correspondant au neurologue Scott Joel
+insert_analyse_EEG (1234567890,1,4); --numero adeli correspondant au neurologue Scott Joel
 
 
 -- Insérer une ligne avec un médecin non-neurologue
 BEGIN
-    INSERT INTO analyse_EEG (id_analyse_eeg, res_electro, num_adeli) VALUES (1,2, '2345678901'); --numero adeli correspondant à un medecin biologiste Hutchinson Jessica
+    insert_analyse_EEG (2345678901,1,2); --numero adeli correspondant à un medecin biologiste Hutchinson Jessica
 EXCEPTION
     WHEN OTHERS THEN
-        DBMS_OUTPUT.PUT_LINE('Erreur : le médecin n est pas neurologue' || SQLCODE);
+    RAISE_APPLICATION_ERROR(-20001,'Erreur : le médecin n est pas neurologue');
 END;
 
 
 -- Insérer une ligne avec un médecin inconnu
 BEGIN
-    INSERT INTO analyse_EEG (id_analyse_eeg, res_electro, num_adeli) VALUES (1,3, '9999999999');
+    insert_analyse_EEG (9999999999,1,3);
 EXCEPTION
     WHEN OTHERS THEN
-        DBMS_OUTPUT.PUT_LINE('Erreur : aucun medecin ne correspond a ce numero adeli ' || SQLCODE );
+        RAISE_APPLICATION_ERROR(-20001,'Erreur : aucun medecin ne correspond a ce numero adeli ');
 END;
-
--- Vérifier que la seule seule ligne valide a bien été insérée dans la table analyse_EEG
-SELECT * FROM analyse_EEG;
 
 END;
 /
